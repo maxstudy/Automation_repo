1f22cde50c1ef643a57e18b8f0b78da36f3457d3
Maxim <maxpipkin1@gmail.com>
2017-11-03 11:24:14 +0300

    task 2 & 4, i didn't have time for task 1 & 3 because I sick

--- Lesson 2 homework/task 4/src/com/company/Main.java
+++ Lesson 2 homework/task 4/src/com/company/Main.java
@@ -7,42 +7,68 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Main {
 
     public static void main(String[] args) {
-//ARRLIST INIT
+
         ArrayList<Integer> arrlist = new ArrayList<Integer>();
         LinkedList<Integer> linlist = new LinkedList<Integer>();
        Model obj = new Model();
        long arr_create_time = obj.initialization(arrlist);
         System.out.println("ArrayList creation time(milliseconds) - " + arr_create_time);
 
-//_______________________________ADD____________________________________
-
             long arr_add_time = obj.bulk_add(arrlist);
         System.out.println("ArrayList insertion time - " + arr_add_time);
 
-//______________________________remove_____________________________________
             long arr_remove_time = obj.bulk_remove(arrlist);
         System.out.println("ArrayList removing time - " + arr_remove_time);
 
-        long arr_search_time = obj.search(arrlist);
+        long arr_search_time = obj.display_search(arrlist);
         System.out.println("ArrayList search time - " + arr_search_time);
 
 
 
 
-
-        System.out.println("LinkedList creation time(milliseconds) - "+ obj.initialization(linlist));
-        // write your code here
+        long lin_create_time = obj.initialization(linlist);
+        System.out.println("LinkedList creation time(milliseconds) - "+ lin_create_time);
 
         long lin_add_time = obj.bulk_add(linlist);
         System.out.println("LinkedList  insertion time - " + lin_add_time);
 
-//______________________________remove_____________________________________
         long lin_remove_time = obj.bulk_remove(linlist);
         System.out.println("LinkedList  removing time - " + lin_remove_time);
 
-        long lin_search_time = obj.search(linlist);
+        long lin_search_time = obj.display_search(linlist);
         System.out.println("LinkedList  search time - " + lin_search_time);
 
+        if (arr_create_time<lin_create_time){
+            System.out.println("ArrayList creation faster than LinkedList on " + (lin_create_time - arr_create_time) + " milliseconds");
+        } else {
+            System.out.println("LinkedList creation faster than ArrayList on " + (arr_create_time - lin_create_time) + " milliseconds");
+
+        }
+
+
+        if (arr_add_time<lin_add_time){
+            System.out.println("ArrayList addition faster than LinkedList on " + (lin_add_time - arr_add_time) + " milliseconds");
+        } else {
+            System.out.println("LinkedList addition faster than ArrayList on " + (arr_add_time - lin_add_time) + " milliseconds");
+
+        }
+
+
+
+        if (arr_remove_time<lin_remove_time){
+            System.out.println("ArrayList remove faster than LinkedList on " + (lin_remove_time - arr_remove_time) + " milliseconds");
+        } else {
+            System.out.println("LinkedList remove faster than ArrayList on " + (arr_remove_time - lin_remove_time) + " milliseconds");
+
+        }
+
+
+        if (arr_search_time<lin_search_time){
+            System.out.println("ArrayList search faster than LinkedList on " + (lin_search_time - arr_search_time) + " milliseconds");
+        } else {
+            System.out.println("LinkedList search faster than ArrayList on " + (arr_search_time - lin_search_time) + " milliseconds");
+
+        }
 
     }
 }
--- Lesson 2 homework/task 4/src/com/company/Model.java
+++ Lesson 2 homework/task 4/src/com/company/Model.java
@@ -10,7 +10,7 @@ public class Model {
    public long initialization(ArrayList<Integer> array){
      int[] arr = new int[10000000];
        for (int i = 0; i < 10000000; i++){
-           arr[i] =  ThreadLocalRandom.current().nextInt(0,  99);
+           arr[i] =  ThreadLocalRandom.current().nextInt(0,  9);
        }
        long start_time = System.currentTimeMillis();
         for (int i = 0; i < 10000000; i++){
@@ -27,14 +27,14 @@ public class Model {
 
        int[] arr = new int[1000];
         for (int i = 0; i < 1000; i++){
-           arr[i] = ThreadLocalRandom.current().nextInt(0, 99);
+           arr[i] = ThreadLocalRandom.current().nextInt(0, 9);
         }
 
         int add_rand_startpoint = ThreadLocalRandom.current().nextInt(0, 10000000);
 
        long start_time = System.currentTimeMillis();
         for (int i = 0; i < 1000; i++){
-            array.add(add_rand_startpoint+i, arr[i]);
+            array.add(5000000, arr[i]);
         }
         long end_time = System.currentTimeMillis();
 
@@ -60,7 +60,7 @@ public class Model {
 
         int k = 0;
         int iter = 0;
-        int search_element = ThreadLocalRandom.current().nextInt(0, 99);
+        int search_element = ThreadLocalRandom.current().nextInt(0, 9);
         long start_time = System.currentTimeMillis();
         while(k < 1000 || iter < 10000000){
             if (array.get(iter) == search_element){
@@ -76,12 +76,30 @@ public class Model {
 
     }
 
+
+
+    public long display_search(ArrayList<Integer> arrlist){
+        int startpoint = ThreadLocalRandom.current().nextInt(0, 10000000);
+        int endpoint = startpoint + 1000;
+        long start_time = System.currentTimeMillis();
+        while ( startpoint < endpoint){
+            int buf = arrlist.get(startpoint);
+            startpoint++;
+        }
+        long end_time = System.currentTimeMillis();
+
+        long diff = end_time - start_time;
+        return diff;
+
+    }
+
+
 //LinkedList methods
     public long initialization(LinkedList<Integer> linklist){
         long start_time = System.currentTimeMillis();
 
         for (int i = 0; i < 10000000; i++){
-            int num = ThreadLocalRandom.current().nextInt(0,  99);
+            int num = ThreadLocalRandom.current().nextInt(0,  9);
             linklist.add(num);
         }
         long end_time = System.currentTimeMillis();
@@ -95,7 +113,7 @@ public class Model {
         long start_time = System.currentTimeMillis();
         int add_rand_startpoint = ThreadLocalRandom.current().nextInt(0, 10000000);
         for (int i = 0; i < 1000; i++){
-            int element = ThreadLocalRandom.current().nextInt(0, 99);
+            int element = ThreadLocalRandom.current().nextInt(0, 9);
             linklist.add(add_rand_startpoint+i, element);
         }
         long end_time = System.currentTimeMillis();
@@ -122,7 +140,7 @@ public class Model {
 
         int k = 0;
         int iter = 0;
-        int search_element = ThreadLocalRandom.current().nextInt(0, 99);
+        int search_element = ThreadLocalRandom.current().nextInt(0, 9);
         long start_time = System.currentTimeMillis();
         while(k < 1000 || iter < 10000000){
             if (linklist.get(iter) == search_element){
@@ -131,6 +149,21 @@ public class Model {
             iter++;
         }
 
+        long end_time = System.currentTimeMillis();
+
+        long diff = end_time - start_time;
+        return diff;
+
+    }
+
+    public long display_search(LinkedList<Integer> linklist){
+        int startpoint = ThreadLocalRandom.current().nextInt(0, 10000000);
+        int endpoint = startpoint + 1000;
+        long start_time = System.currentTimeMillis();
+        while ( startpoint < endpoint){
+            int buf = linklist.get(startpoint);
+            startpoint++;
+        }
         long end_time = System.currentTimeMillis();
 
         long diff = end_time - start_time;
